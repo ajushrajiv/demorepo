8677474b6477d4bfeea4f249de7d837482d90b3b
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2023-12-15 15:32:30 +0100

    added functions that displays value from dictionary, print keys and the print values

--- dictionary.py
+++ dictionary.py
@@ -8,3 +8,40 @@ def addItem(key, value):
 
 addItem("Horst", "Kurt")
 print(dictionary["Horst"])
+
+#printValue
+print('Schlüssel', dictionary)
+
+
+#valueIsEqual
+def check_key(key, value, dict):
+    return dict.get(key) == value
+
+check_value = check_key('Hotel', 'Trivagu', dictionary)
+print(check_value)
+
+#deleteItem
+
+del dictionary["Horst"]
+
+check_value = check_key('Horst', 'Kurt', dictionary)
+print(check_value)
+
+#printKeys
+
+print("Keys are listed below")
+def printKeys(dictionary):
+    for i in dictionary.keys():
+        print(i)
+
+printKeys(dictionary)
+
+
+#printValues
+
+print("Values are listed below")
+def printValues(dictionary):
+    for i in dictionary.values():
+        print (i)
+
+printValues(dictionary)
